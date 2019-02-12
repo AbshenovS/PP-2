e620531ec96d6ca81e8b18d83f31b4e2e278e666
Abshenov Sagyndyk <Abshenovs.21@gmail.com>
2019-02-12 19:28:25 +0600

    ..

--- Week2/Task4/Program.cs
+++ Week2/Task4/Program.cs
@@ -11,23 +11,21 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            string fldrname = @"C:\Users\hp\Desktop\Test\aw";
-            /*string path = Path.Combine(fldrname, "NewFolder");
+            /*string fldrname = @"C:\Users\hp\Desktop\Test\aw";
+            string path = Path.Combine(fldrname, "NewFolder");
             path = Path.Combine(path, "2-newfolder");
-            Directory.CreateDirectory(path);*/
+            Directory.CreateDirectory(path);
             string newfile="Newfilenm.txt";
             string path = Path.Combine(fldrname, newfile);
-            File.Create(path);
-            Console.WriteLine(path);
-            //string fldr = @"C:\Users\hp\Desktop\Test\aw";
-            string newfldr = @"C:\Users\hp\Desktop\Test\Qw";
-            //string newfile = "Filename.txt";
-            //string path2 = Path.Combine(path1, newfile);
-            string newpath = Path.Combine(newfldr, newfile);
+            File.Create(path);*/
+            string filename = "Newfile.txt";
+            string fldr = @"C:\Users\hp\Desktop\Test\NewFolder\2-newfolder";
+            string newfldr = @"C:\Users\hp\Desktop\Test\Secondfldr";
+            string path = Path.Combine(fldr, filename);
+            string newpath = Path.Combine(newfldr, filename);
             File.Copy(path, newpath, true);
-
-
-            
+            File.Delete(path);
+            Console.WriteLine("File is copied and original file is deleted succesfully");
         }
     }
 }
--- Week3/Task1/Program.cs
+++ Week3/Task1/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task1
 {
+    
+    
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
