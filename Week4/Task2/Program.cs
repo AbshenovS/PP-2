c2e7f75a4dd359d7e17b422caa7678d45fb161b1
Abshenov Sagyndyk <Abshenovs.21@gmail.com>
2019-02-22 14:15:29 +0600

    Upd

--- Week4/Task2/Program.cs
+++ Week4/Task2/Program.cs
@@ -30,7 +30,12 @@ namespace Task2
             else if (points <= 94 && points >= 90) return "-A";
             else if (points <= 89 && points >= 85) return "B+";
             else if (points <= 84 && points >= 80) return "B";
-
+            else if (points <= 79 && points >= 75) return "B-";
+            else if (points <= 74 && points >= 70) return "C+";
+            else if (points <= 69 && points >= 65) return "C";
+            else if (points <= 64 && points >= 60) return "C-";
+            else if (points <= 59 && points >= 55) return "D+";
+            else if (points <= 54 && points >= 50) return "D";
             return "F";
         }
     }
@@ -54,6 +59,14 @@ namespace Task2
             Mark a4 = new Mark(86);
             a4.letter = a4.getLetter();
             l.Add(a4);
+
+            Mark a5 = new Mark(53);
+            a5.letter = a5.getLetter();
+            l.Add(a5);
+
+            Mark a6 = new Mark(26);
+            a6.letter = a6.getLetter();
+            l.Add(a6);
 
             FileStream fs = new FileStream(@"C:\Users\hp\Desktop\Test\seri1.txt", FileMode.OpenOrCreate, FileAccess.Write);
             ToSerialize(l, fs);
